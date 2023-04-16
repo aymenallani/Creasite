606779532de35834980c013604ce504bb5a699ba
aymenallani <aymen.allani9@gmail.com>
2023-04-17 00:51:57 +0100

    commit

--- src/main/java/com/Projet/usermanagement/controller/UserController.java
+++ src/main/java/com/Projet/usermanagement/controller/UserController.java
@@ -2,14 +2,20 @@ package com.Projet.usermanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Projet.usermanagement.dto.RegistrationDto;
+import com.Projet.usermanagement.dto.UserDto;
 import com.Projet.usermanagement.entity.AppUser;
 import com.Projet.usermanagement.mapper.RegistrationMapper;
+import com.Projet.usermanagement.mapper.UserMapper;
 import com.Projet.usermanagement.service.UserService;
+import com.Projet.websitemanagement.dto.TemplateDto;
+import com.Projet.websitemanagement.entity.Template;
 
 import jakarta.validation.Valid;
 
@@ -19,11 +25,23 @@ public class UserController {
 	private UserService userService;
 	@Autowired
 	private RegistrationMapper registrationMapper;
+	@Autowired
+	private UserMapper userMapper;
 	
 	@PostMapping("/register")
 	public ResponseEntity<AppUser> registerUser(@Valid @RequestBody RegistrationDto userDto) {
 		AppUser user = registrationMapper.unMap(userDto);
 		return ResponseEntity.ok(userService.Registration(user));
+	}
+	
+	@GetMapping("users/{id}")
+	public ResponseEntity<?> findById(@PathVariable long id){
+
+		AppUser user = userService.findById(id);
+
+		UserDto dto = userMapper.map(user);
+
+		return ResponseEntity.ok(dto);
 	}
 
 }
--- src/main/java/com/Projet/usermanagement/service/UserService.java
+++ src/main/java/com/Projet/usermanagement/service/UserService.java
@@ -1,13 +1,17 @@
 package com.Projet.usermanagement.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.Projet.base.service.BaseService;
+import com.Projet.error.DataNotFoundException;
 import com.Projet.usermanagement.entity.AppUser;
 import com.Projet.usermanagement.repository.UserRepository;
+import com.Projet.websitemanagement.entity.Template;
 
 @Service
 public class UserService  extends BaseService<AppUser, Long>{
@@ -30,5 +34,11 @@ public class UserService  extends BaseService<AppUser, Long>{
 		} catch (Exception e) {
 			throw new RuntimeException("Failed to register user");
 		}
+	}
+	
+	public AppUser findById(Long id) {
+		Optional<AppUser> optionalAppUser = userRepository.findById(id);
+		AppUser user = optionalAppUser.orElseThrow(() -> new DataNotFoundException("user not found"));
+        return user;
 	}
 }
